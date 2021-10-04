50f02e931fad3b6192fb1c1ddfa8e811c41bacae
DianaSmarandoiu <73424669+DianaSmarandoiu@users.noreply.github.com>
2021-10-04 21:57:01 +0300

    Add files via upload

---
+++ desene.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 17:56:19 2021
+
+@author: Didi
+"""
+
+
+import turtle
+
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(200)
+turtle.forward(100)
+turtle.right(300)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
